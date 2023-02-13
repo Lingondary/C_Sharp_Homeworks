51984d5cea75e14a43ee3856d085701ee821cee6
Sergey Shevchenko <lingondary@gmail.com>
2023-02-13 15:05:00 +0700

    Task 2 Complete

---
+++ HW_1/Task 2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+int max;
+Console.Clear();
+Console.WriteLine("Программа сравнивает 3 числа (А, B и C) и выдаёт максимальное.");
+
+Console.WriteLine("Введите число A:");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B:");
+int B = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число C:");
+int C = Convert.ToInt32(Console.ReadLine());
+
+if(A < B)
+{
+    max = B;
+}
+
+else
+{
+    max = A;
+}
+
+if(max < C)
+{
+    max = C;
+}
+
+Console.WriteLine("Наибольшее число: " + max);
