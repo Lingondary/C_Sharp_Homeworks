95433a30326f07f024f9a8856ea7cd21debbf96c
Sergey Shevchenko <lingondary@gmail.com>
2023-02-13 13:25:52 +0700

    init commit, ignore list added

---
+++ HW_1/Task 1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+
+Console.WriteLine("Проверка двух чисел A и B и нахождение большего из них.");
+
+Console.WriteLine("Введите число A:"); 
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B:"); 
+int B = Convert.ToInt32(Console.ReadLine());
+
+    if (A > B)
+    {
+        Console.WriteLine("A > B"); 
+    }
+
+    else
+    {
+        Console.WriteLine("A < B"); 
+    }
