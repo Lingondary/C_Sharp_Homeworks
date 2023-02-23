8bb7c1b2c434a6c5f9d1b66ea1aae572dd71fd18
Sergei Shevchenko <lingondary@gmail.com>
2023-02-23 14:40:02 +0700

    HW_2 started

---
+++ HW_2/Task 1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+Console.WriteLine("Програма показывает вторуб цифру числа.");
+
+Console.WriteLine("Введите число.");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int FindSecondChar(int num)
+{
+    int result = 0;
+
+    result = (num/10) % 10;
+
+    return result;
+}   
+
+Console.WriteLine(FindSecondChar(a));
