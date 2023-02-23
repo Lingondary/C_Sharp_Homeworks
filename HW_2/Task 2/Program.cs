9a48fba5f664c80d722343d027c42778116362a3
Sergei Shevchenko <lingondary@gmail.com>
2023-02-23 15:39:00 +0700

    Task 2 completed

---
+++ HW_2/Task 2/Program.cs
@@ -0,0 +1,35 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Clear();
+Console.WriteLine("Программа выводит третью цифру числа или сообщает o её отсутствии.");
+
+Console.WriteLine("Введите число.");
+int a = Convert.ToInt32(Console.ReadLine());
+
+string FindThirdChar(int num)
+{
+    int result = 0;
+    if (num > 99)
+    {
+        result = num % 10; 
+        return result.ToString();
+    }
+
+    if (num > 999)
+    {
+        result = (num % 100) / 10; 
+        return result.ToString();
+    }
+
+    if (num > 9999)
+    {
+        return "Число слишком большое.";
+    }
+
+    else
+    {
+        return "Это число не имеет третей цифры.";
+    }
+}   
+
+Console.WriteLine(FindThirdChar(a));
