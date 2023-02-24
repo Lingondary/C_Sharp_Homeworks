e9e018af7d79cd28f90a09607e1004264fce6e3f
Sergei Shevchenko <lingondary@gmail.com>
2023-02-24 15:12:06 +0700

    Task 3 completed

---
+++ HW_3/Task 3/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Clear();
+Console.WriteLine("Программа принимает на вход число N и выдаёт таблицу кубов чисел от одного до N.");
+Console.WriteLine("Введите число N.");
+
+int N = Convert.ToInt32(Console.ReadLine());
+int counter = 0;
+
+while (counter < N)
+{
+    Console.Write(Math.Pow(counter, 2) + " ");
+    counter++;
+}
