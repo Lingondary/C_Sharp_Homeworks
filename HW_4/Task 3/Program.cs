644dbe7fe6ca461d81d2005239b9d44006735fe2
Sergei Shevchenko <lingondary@gmail.com>
2023-02-26 13:40:32 +0700

    Task 4 complete

---
+++ HW_4/Task 3/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из m элементов и выводит их на экран.
+
+Console.Clear();
+Console.WriteLine("Программа создаёт и выводит случайный массив.");
+
+Random rnd = new Random();
+int [] array = new int[20];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(1, 100);
+    Console.Write(array[i] + " ");
+};
