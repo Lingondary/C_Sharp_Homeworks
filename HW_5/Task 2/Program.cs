10ab59f91303e377151c33e78ca2b0c7f133cc8c
Sergei Shevchenko <lingondary@gmail.com>
2023-03-03 12:37:45 +0700

    Task 2 Completed

---
+++ HW_5/Task 2/Program.cs
@@ -0,0 +1,37 @@
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+void FillArray(int[] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        array[i] = new Random().Next(100, 999);
+    }
+}
+void CoutArray(int[] array)
+{
+    Console.WriteLine("Заполненный массив:");
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+int OddSum(int[] array)
+{
+    int sum = 0;
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        if(array[i] % 2 != 0)
+        {
+            sum = sum + array[i];
+        }
+    } 
+    return sum;
+}
+
+int [] massive = new int [10];
+
+Console.Clear();
+FillArray(massive);
+CoutArray(massive);
+Console.WriteLine();
+Console.WriteLine($"Сумма нечётных элементов массива: {OddSum(massive)}");
