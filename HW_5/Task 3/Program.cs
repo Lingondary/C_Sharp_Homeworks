5c24862780aee99a33b51ed2fbb88fd9283fda9c
Sergei Shevchenko <lingondary@gmail.com>
2023-03-03 12:53:27 +0700

    Task 3 Completed

---
+++ HW_5/Task 3/Program.cs
@@ -0,0 +1,53 @@
+﻿//Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+void FillArray(double[] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        array[i] = new Random().Next(100, 999);
+    }
+}
+void CoutArray(double[] array)
+{
+    Console.WriteLine("Заполненный массив:");
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+double MaxNum(double[] array)
+{
+    double maxNum = array[1];
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        if(array[i] > maxNum)
+        {
+            maxNum = array[i];
+        }
+    }
+    return maxNum;
+}
+double MinNum(double[] array)
+{
+    double minNum = array[1];
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        if(array[i] < minNum)
+        {
+            minNum = array[i];
+        }
+    }
+    return minNum;
+}
+double Diff(double[] array)
+{
+    double diff = MaxNum(array) - MinNum(array);
+    return diff;
+}
+
+double [] massive = new double [10];
+Console.Clear();
+FillArray(massive);
+CoutArray(massive);
+Console.WriteLine();
+Console.WriteLine($"Разница между максимальным и минимальным элементом массива: {Diff(massive)}");
