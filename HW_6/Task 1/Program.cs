41840adb1c536048a781b3e5d6e36143008e8392
Sergei Shevchenko <lingondary@gmail.com>
2023-03-07 13:57:36 +0700

    Task 1 completed

---
+++ HW_6/Task 1/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Программа выводит на экран количество введёных в массив чисел больше 0.");
+Console.WriteLine("Введите 7 чисел, нажимая Enter после ввода кажого числа.");
+
+void fillArray(int[] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        Array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+int aboveZeroCounter(int[] Array)
+{
+    int counter = 0;
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        if(Array[i] > 0)
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
+
+int[] Array = new int [7];
+
+fillArray(Array);
+
+Console.Write("Количество чисел больше нуля: ");
+Console.Write($"{aboveZeroCounter(Array)}");
